4e6f151cd77a304e29375ed13dfc9957b417b7bb
fjcamillo <fjcamillo.dev@gmail.com>
2017-09-09 23:43:20 +0800

    working video stream on flask using generators

--- camera.py
+++ camera.py
@@ -5,5 +5,5 @@ class Camera (object):
     def __init__(self):
         self.frames = [ open(f +  '.jpg', 'rb').read() for f in ['1', '2', '3']]
 
-    def get_frames(self):
+    def get_frame(self):
         return self.frames[int(time()) %3]
